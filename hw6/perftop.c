5102e78223408e36684b6adb8f21a434d564afbd
rherna70 <rherna70@uic.edu>
2024-02-17 12:49:27 -0700

    remove unused code

--- hw6/perftop.c
+++ hw6/perftop.c
@@ -10,23 +10,6 @@ static int perftop_show(struct seq_file *m, void *v) {
 
 EXPORT_SYMBOL(perftop_show);
 
-/////////////////// HASH TABLE BEGIN ///////////////////////////
-/*
- * Let's make our hash table have 2^10 = 1024 bins
- * */
-// #define MY_HASH_TABLE_BINS 10
-// static DEFINE_HASHTABLE(myhtable, MY_HASH_TABLE_BINS);
-
-/* Hashtable entry struct */
-// struct hentry {
-// 	int val;
-// 	struct hlist_node hash;
-// };
-// EXPORT_SYMBOL(myhtable);
-// EXPORT_SYMBOL(hentry);
-/////////////////// HASH TABLE END ///////////////////////////
-
-
 static int perftop_open(struct inode *inode, struct file *file) {
     return single_open(file, perftop_show, NULL);
 }
