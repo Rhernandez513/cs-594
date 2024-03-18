6048a92d02f76a60e3ec014030692f3fbffba1e5
Robert David Hernandez <rherna70@uic.edu>
2024-03-17 20:13:39 -0600

    this change boots the qemu vm with a virtual device correctly

--- hw9/run-bookworm.sh
+++ hw9/run-bookworm.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-QEMU_BIN=${PWD}/qemu-8.2.1/build/qemu-system-x86_64
+# QEMU_BIN=${PWD}/qemu-8.2.1/build/qemu-system-x86_64
+QEMU_BIN=/users/rherna70/code/qemu-8.2.1/build/qemu-system-x86_64
 NCPU=4
 MEMSIZE=4G
 
-KNL_SRC=${PWD}/linux-6.1.72
+# KNL_SRC=${PWD}/linux-6.1.72
+KNL_SRC=/users/rherna70/code/linux-6.1.72
 BZIMAGE=${KNL_SRC}/arch/x86/boot/bzImage
-CMDLINE="nokaslr console=ttyS0 root=/dev/vda1"
+# CMDLINE="nokaslr console=ttyS0 root=/dev/vda1"
+CMDLINE="nokaslr console=ttyS0 root=/dev/sda"
 
 # -drive if=virtio,format=qcow2,file=ubuntu2204.img \
 
@@ -14,6 +17,6 @@ sudo ${QEMU_BIN} \
   -smp ${NCPU} -m ${MEMSIZE} \
   -device virtio-net-pci,netdev=net0 \
   -netdev user,id=net0,hostfwd=tcp::2222-:22 \
-  -drive file=./image/bookworm.img,format=qcow2 \
-  -enable-kvm \
-  -kernel ${BZIMAGE} -append "${CMDLINE}"
+  -drive file=/users/rherna70/code/image/bookworm.img,format=raw \
+  -kernel ${BZIMAGE} -append "${CMDLINE}" \
+  -device my_rng
