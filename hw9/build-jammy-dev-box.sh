eb8666f1710e122d0b78543b96d3e4ac76e39495
Robert David Hernandez <rherna70@uic.edu>
2024-03-20 17:43:45 -0600

    shell script for building a linux kernel dev box on ubuntu jammy

---
+++ hw9/build-jammy-dev-box.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/sh
+
+# build-jammy-dev-box.sh
+# This script is used to setup a development environment for Ubuntu 22.04 (Jammy Jellyfish)
+# For building the Linux Kernel, QEMU from source code, and other development tools
+
+# Taskfile.dev
+sudo sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b /usr/local/bin
+
+
+# Github Command Line Interface
+sudo mkdir -p -m 755 /etc/apt/keyrings && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
+sudo apt update
+sudo apt install gh -y
+
+
+# For building Linux Kernel from Source
+sudo apt install -y build-essential git libssl-dev flex bison wget pkg-config libelf-dev
+sudo apt install -y qemu-system-x86 debootstrap
+
+# Linux Headers and Source for browsing and debugging
+sudo apt install -y linux-headers-$(uname -r)
+sudo apt install -y linux-source
+
+
+# ZSH and Oh-My-ZSH
+sudo apt install -y tmux zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+# For building QEMU from source
+sudo apt install -y build-essential git bc libelf-dev libssl-dev flex bison meson wget python3 python3-venv ninja-build pkg-config libglib2.0-dev libslirp-dev
+
+
+# Update packages and perform cleanup
+sudo apt upgrade -y --allow-downgrades
+sudo apt autoremove
+
+# EOF 
