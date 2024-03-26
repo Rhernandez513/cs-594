6f0f1f86c34844866b5b7ef775b0b90a3fe932ac
Robert David Hernandez <rherna70@uic.edu>
2024-03-25 22:56:18 -0600

    helper scripts

--- hw9/setup-vm.sh
+++ hw9/setup-vm.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-mknod /dev/my_rng_driver c 250 0
-ls -l /dev/my_rng_driver
-
-mknod /dev/lkp_enc_driver c 251 0
+mknod /dev/lkp_enc_driver c 250 0
 ls -l /dev/lkp_enc_driver
 
 apt install build-essential zsh
